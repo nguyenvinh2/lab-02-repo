f2fc8c26dc907f25af2f9a866091aa7f7ccc2986
Vinh Nguyen <nguyenv2@outlook.com>
2019-04-24 09:29:00 -0700

    render horns

--- js/app.js
+++ js/app.js
@@ -1,2 +1,43 @@
 'use strict';
 
+function Horn(hornObject) {
+  this.title = hornObject.title;
+  this.description = hornObject.description;
+  this.image = hornObject.image_url;
+  this.keyword = hornObject.keyword;
+  this.horns = hornObject.horns;
+}
+
+function HornCollection() {
+  this.hornList = [];
+
+  this.getHorns = () => {
+    $.get('data/page-1.json', 'json')
+      .then(data => {
+        data.forEach(animal => {
+          this.hornList.push(new Horn(animal));
+        });
+        this.renderHorns();
+      });
+  };
+
+  this.renderHorns = () => {
+    this.hornList.forEach(horn => {
+      const $imgDiv = $(`<div id=${horn.title}></div>`);
+      $imgDiv.attr('id', horn.title);
+      const $img = $('<img/>');
+      $img.attr('src', horn.image);
+      $img.appendTo($imgDiv);
+      $imgDiv.appendTo($('#horns'));
+    });
+
+  };
+}
+
+const Horns = new HornCollection();
+
+Horns.getHorns();
+
+console.log(Horns.hornList);
+
+
